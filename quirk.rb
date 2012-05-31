5681c1b7426d4b2a7ce9ba6ca076c924b6362fc5
Hugh Bien <hugh@hughbien.com>
2012-05-31 08:35:46 -0700

    Upgrade to v0.0.4.

--- quirk.rb
+++ quirk.rb
@@ -2,7 +2,7 @@ require 'date'
 require 'colorize'
 
 module Quirk
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
   QUIRKFILE = ENV['QUIRKFILE'] || "#{ENV['HOME']}/.quirk"
   EDITOR = ENV['EDITOR'] || 'vi'
 
